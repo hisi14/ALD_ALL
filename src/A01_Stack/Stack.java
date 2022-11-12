3a845005117c305381e01c5a4ecb009bec6a4aa7
Lukas <lukas.la777@gmail.com>
2022-11-12 14:34:39 +0100

    init + finish Stack

--- src/A01_Stack/Stack.java
+++ src/A01_Stack/Stack.java
@@ -3,15 +3,22 @@ package A01_Stack;
 
 public class Stack<T>
 {
-	 private Node<T> first;
+	 private Node<T> last;
+     private int counter = 0;
     /**
      * Oberstes Element entfernen und zurückliefern.
      * Existiert kein Element, wird eine Exception ausgelöst.
      * @throws StackEmptyException 
      */
     public T pop() throws StackEmptyException {
+        if(last == null)
+            throw new StackEmptyException();
 
-    	return null;
+        T returnValue = last.getData();
+        last = last.getNext();
+
+        counter --;
+        return returnValue;
     }
     
     /**
@@ -20,6 +27,10 @@ public class Stack<T>
      */
     public void push(T i) {
 
+        Node<T> helper = new Node<>(i);
+        helper.setNext(last);
+        last = helper;
+        counter ++;
     }
     
     /**
@@ -27,6 +38,6 @@ public class Stack<T>
      * @return
      */
     public int getCount() {
-    	return 0;
+    	return counter;
     }
 }
